355328b4e7efd7ac73beeccaabe014383ee46a6d
Robotarians <cwtech2959@gmail.com>
2019-01-26 17:30:41 -0500

    File reorganization

--- SharedNames.h
+++ SharedNames.h
@@ -1,4 +1,6 @@
-#define TABLE_NAME      "IATable"
-#define TARGET_COORDS   "TargetCoords"
-#define FRAME_NUMBER    "FrameNumber"
-#define PROCESSING_TARGETS "ProcessingTargets"
+// The name of the NetworkTables table name to use
+#define TABLE_NAME          "IATable"   
+
+// Network table key names
+#define FRAME_NUMBER        "FrameNumber"
+#define PROCESSING_TARGETS  "ProcessingTargets"
